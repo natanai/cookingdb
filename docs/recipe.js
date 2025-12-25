5d83f4470c0bebd4d01d771af64d8b8f28df76f5
natanai <109430106+natanai@users.noreply.github.com>
2025-12-24 21:05:09 -0600

    Emphasize ingredients in recipe steps

--- docs/recipe.js
+++ docs/recipe.js
@@ -496,7 +496,7 @@ function renderSteps(recipe, state) {
     section.lines.forEach((line) => {
       const li = document.createElement('li');
       li.className = 'step-item';
-      li.textContent = line.text;
+      li.innerHTML = line.text;
       steps.appendChild(li);
     });
   });
